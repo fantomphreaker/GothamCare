f344eb5fd966fe121084a2307019df7c5df9f955
fantomphreaker <navaneethgopal4991@gmail.com>
2021-01-15 10:26:58 +0530

    Created Outlet class and test migration done to postgreSQL server successfully

---
+++ GothamCareApi/GothamCareApi/Models/GothamCareApiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GothamCareApi.Models
+{
+    public class GothamCareApiContext : DbContext
+    {
+        public GothamCareApiContext(DbContextOptions<GothamCareApiContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Outlet> Outlets { get; set; }
+    }
+}
---
+++ GothamCareApi/GothamCareApi/Models/Outlet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GothamCareApi.Models
+{
+    public class Outlet
+    {
+        public int Id { get; set; }
+        
+        public string OutletName { get; set; }
+        public string StreetName { get; set; }
+        public string Landmark { get; set; }
+        public int TotalAvailableFoodPackets { get; set; }
+        public string FoodType { get; set; }
+        public int RequiredNoOfVolunteers { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
